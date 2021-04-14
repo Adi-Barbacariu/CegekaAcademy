6c5185648073fc066244a22e8b093fa9d5694146
Adi Barbacariu <barbacariu.adi@gmail.com>
2021-04-14 09:33:22 +0300

    Step 1 - final

--- Homework08/CegekaAcademy2021.Session9Homework-main/src/components/Album/AlbumForm.jsx
+++ Homework08/CegekaAcademy2021.Session9Homework-main/src/components/Album/AlbumForm.jsx
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { Header, Button, Modal, Input, Dropdown } from "semantic-ui-react";
 
-function AlbumForm() {
-  return <div>album form</div>;
+const InputSelectPhotos = ({ photos, handleChange }) => {
+  const options = [];
+  for (let [key, value] of Object.entries(photos)) {
+    const option = {
+      key: key,
+      text: value.title,
+      value: key,
+      image: { avatar: false, src: value.url },
+    };
+    options.push(option);
+  }
+
+  return (
+    <Dropdown
+      placeholder="Select photos"
+      name="photos"
+      fluid
+      multiple
+      search
+      selection
+      options={options}
+      onChange={handleChange}
+    />
+  );
+};
+
+function AlbumForm({ open, handleOpen, photos, setOpen, createItem }) {
+  const [itemData, setItemData] = useState({});
+
+  const handleClose = () => {
+    setItemData({});
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setItemData({ ...itemData, [e.target.name]: value });
+  };
+
+  const handleDropdownChange = (e, data) => {
+    setItemData({ ...itemData, photosIds: data.value });
+  };
+
+  const handleAdd = () => {
+    createItem(itemData);
+    handleClose();
+  };
+
+  return (
+    <Modal onClose={handleClose} onOpen={handleOpen} open={open}>
+      <Modal.Header>Add a new album</Modal.Header>
+      <Modal.Content image>
+        <Modal.Description>
+          <Header as="h3">Name</Header>
+          <Input
+            fluid
+            name="name"
+            placeholder="Enter a meaningful title here"
+            onChange={handleChange}
+          />
+          <Header as="h3">Description</Header>
+          <Input
+            fluid
+            name="description"
+            placeholder="Enter a good description here"
+            onChange={handleChange}
+          />
+          <Header as="h3">Tags</Header>
+          <Input
+            fluid
+            name="tags"
+            placeholder="Enter tags separated by a space"
+            onChange={handleChange}
+          />
+          <Header as="h3">Photos</Header>
+          <InputSelectPhotos
+            photos={photos}
+            handleChange={handleDropdownChange}
+          />
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button
+          content="Add"
+          labelPosition="right"
+          icon="checkmark"
+          onClick={handleAdd}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
 export default AlbumForm;
--- Homework08/CegekaAcademy2021.Session9Homework-main/src/components/Photo/PhotoForm.jsx
+++ Homework08/CegekaAcademy2021.Session9Homework-main/src/components/Photo/PhotoForm.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { Header, Button, Modal, Input } from "semantic-ui-react";
 
-function PhotoForm() {
-  return <div>photoform</div>;
+function PhotoForm({ open, handleOpen, setOpen, createItem }) {
+  const [itemData, setItemData] = useState({});
+
+  const handleClose = () => {
+    setItemData({});
+    setOpen(false);
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setItemData({ ...itemData, [e.target.name]: value });
+  };
+
+  const handleAdd = () => {
+    createItem(itemData);
+    handleClose();
+  };
+
+  return (
+    <Modal onClose={handleClose} onOpen={handleOpen} open={open}>
+      <Modal.Header>Add a new photo</Modal.Header>
+      <Modal.Content image>
+        <Modal.Description>
+          <Header as="h3">Title</Header>
+          <Input
+            fluid
+            name="title"
+            placeholder="Enter a meaningful title here"
+            onChange={handleChange}
+          />
+          <Header as="h3">Description</Header>
+          <Input
+            fluid
+            name="description"
+            placeholder="Enter a good description here"
+            onChange={handleChange}
+          />
+          <Header as="h3">URL</Header>
+          <Input
+            fluid
+            name="url"
+            placeholder="Enter the URL here"
+            onChange={handleChange}
+          />
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button
+          content="Add"
+          labelPosition="right"
+          icon="checkmark"
+          onClick={handleAdd}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
+  );
 }
 
 export default PhotoForm;
